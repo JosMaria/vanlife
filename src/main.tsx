c9af4184dfafcc89494b185f0fa91d98674b1f6d
JosMaria <josmariaguilar@gmail.com>
2024-11-08 19:06:36 -0400

    refactor code

--- src/main.tsx
+++ src/main.tsx
@@ -4,25 +4,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import About from './pages/About';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <h1>Hello, React router!</h1>
-  );
-}
-
-function About() {
-  return (
-    <h1>About page</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
