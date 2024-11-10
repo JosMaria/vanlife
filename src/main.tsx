a769e4ea590aadfc97964c775c536e005382869c
JosMaria <josmariaguilar@gmail.com>
2024-11-10 15:56:38 -0400

    decorate vans page

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import './index.css';
+import './server';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
@@ -6,17 +7,22 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import About from './pages/About';
 import Home from './pages/Home';
+import Vans from './pages/Vans';
 
 function App() {
   return (
     <BrowserRouter>
       <header className='bg-amber-50 flex justify-between items-center px-2 py-4'>
         <Link className='text-xl font-extrabold' to='/'>#VANLIFE</Link>
-        <nav className='text-sm font-medium'><Link to='/about'>About</Link></nav>
+        <nav className='text-sm font-medium flex gap-5'>
+          <Link to='/about'>About</Link>
+          <Link to='/vans'>Vans</Link>
+        </nav>
       </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Vans.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+type VanType = {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+  type: "simple" | "rugged" | "luxury";
+}
+
+export default function Vans() {
+  const [vans, setVans] = React.useState<VanType[]>([]);
+
+  React.useEffect(() => {
+    fetch('/api/vans')
+      .then(res => res.json())
+      .then((data: { vans: VanType[] }) => setVans(data.vans));
+  }, []);
+
+
+  const vansElements = vans.map(van => (
+    <div className='bg-orange-100 max-w-32 p-1.5 flex flex-col gap-1 leading-tight text-sm' key={van.id}>
+      <img src={van.imageUrl} alt={van.name} />
+      <h3 className='font-bold'>{van.name}</h3>
+      <p className='font-bold'>${van.price}<span className='font-normal'>/day</span></p>
+      <i className={`${van.type === 'simple' ? 'bg-orange-600' : van.type === 'rugged' ? 'bg-emerald-600' : 'bg-slate-700'} font-semibold text-white w-fit px-3 py-1 rounded-sm`}>{van.type}</i>
+    </div>
+  ));
+
+  return (
+    <div className='flex flex-col gap-2 p-2'>
+      <h1 className='font-bold text-xl'>Explore our van options</h1>
+      <section className='w-full flex flex-wrap justify-around gap-5'>
+        {vansElements}
+      </section>
+    </div>
+  )
+}
