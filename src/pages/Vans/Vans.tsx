6bf072b3d21d56d7b015a9e1cb6906d0a2029484
JosMaria <josmariaguilar@gmail.com>
2024-11-13 14:23:33 -0400

    add useSearchParams

--- src/pages/Vans/Vans.tsx
+++ src/pages/Vans/Vans.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 import { VanType } from '../../types';
 
 export default function Vans() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [vans, setVans] = React.useState<VanType[]>([]);
+
+  const typeFilter = searchParams.get('type');
 
   React.useEffect(() => {
     fetch('/api/vans')
@@ -12,13 +15,16 @@ export default function Vans() {
       .then((data: { vans: VanType[] }) => setVans(data.vans));
   }, []);
 
-  const vansElements = vans.map(van => (
-    <div className='bg-orange-100 max-w-32 p-1.5 leading-tight text-sm' key={van.id}>
+
+  const displayedVans = typeFilter ? vans.filter(van => van.type === typeFilter.toLowerCase()) : vans;
+
+  const vansElements = displayedVans.map(van => (
+    <div className='bg-orange-100 p-1.5 leading-tight text-sm' key={van.id}>
       <Link to={`/vans/${van.id}`} className='flex flex-col gap-1'>
-        <img src={van.imageUrl} alt={van.name} />
+        <img src={van.imageUrl} alt={van.name} width={130} />
         <h3 className='font-bold'>{van.name}</h3>
         <p className='font-bold'>${van.price}<span className='font-normal'>/day</span></p>
-        <p className={`${van.type === 'simple' ? 'bg-orange-500' : van.type === 'rugged' ? 'bg-emerald-600' : 'bg-slate-600'} font-semibold text-white w-fit px-3 py-1 rounded-sm`}>{van.type}</p>
+        <p className={`${van.type === 'simple' ? 'bg-orange-500' : van.type === 'rugged' ? 'bg-emerald-600' : 'bg-slate-600'} font-semibold text-white w-fit px-2 py-0.5 rounded-sm text-xs`}>{van.type}</p>
       </Link>
     </div>
   ));
@@ -26,7 +32,13 @@ export default function Vans() {
   return (
     <div className='flex flex-col gap-2 p-2'>
       <h1 className='font-bold text-xl'>Explore our van options</h1>
-      <section className='w-full flex flex-wrap justify-around gap-5'>
+      <div className='text-xs flex gap-3'>
+        <button onClick={() => setSearchParams({ type: 'simple' })}>Simple</button>
+        <button onClick={() => setSearchParams({ type: 'luxury' })}>Luxury</button>
+        <button onClick={() => setSearchParams({ typ: 'rugged' })}>Rugged</button>
+        <button onClick={() => setSearchParams({})}>Clear Filters</button>
+      </div>
+      <section className='w-full flex flex-wrap justify-between gap-5'>
         {vansElements}
       </section>
     </div>
